10c53c4ed5a606de46096ff4f91fd696d89f428e
professorjiwon <professorjiwon@gmail.com>
2025-03-21 18:24:55 +0900

    03.21_8교시

--- mybatis/src/main/java/com/mybatis/board/controller/BoardDetailServlet.java
+++ mybatis/src/main/java/com/mybatis/board/controller/BoardDetailServlet.java
@@ -11,6 +11,9 @@ public class BoardDetailServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 조회수 증가 시키기
+		
+		// 조회수 증가가 잘 되었으면 검색하여 boardDetailView.jsp로 가져가기
 		
 		request.getRequestDispatcher("WEB-INF/views/board/boardDetailView.jsp").forward(request, response);
 	}
